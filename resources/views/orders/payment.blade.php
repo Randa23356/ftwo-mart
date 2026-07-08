3040207da4e2f92af6886f490dfc0359ef14edfe
Randa23356 <randamoonton@gmail.com>
2026-07-08 21:43:59 +0800

    fix

--- resources/views/orders/payment.blade.php
+++ resources/views/orders/payment.blade.php
@@ -120,7 +120,7 @@
 </div>
 
 <!-- Payment Script -->
-<script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('services.midtrans.client_key') }}"></script>
+<script src="{{ config('services.midtrans.is_production') ? 'https://app.midtrans.com/snap/snap.js' : 'https://app.sandbox.midtrans.com/snap/snap.js' }}" data-client-key="{{ config('services.midtrans.client_key') }}"></script>
 
 <script>
 document.getElementById('pay-button').addEventListener('click', function() {
