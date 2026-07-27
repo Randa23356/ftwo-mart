aed2130da231fdb701ac3c63df17b19d2e31f880
Randa23356 <randamoonton@gmail.com>
2026-07-27 21:16:42 +0800

    update

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -31,7 +31,7 @@ class GoogleController extends Controller
                 return redirect()->intended(route('home'));
             }
 
-            // Check if email already exists (registered normally)
+            // Check if email already exists (registered normally))
             $existingUser = User::where('email', $googleUser->email)->first();
 
             if ($existingUser) {
